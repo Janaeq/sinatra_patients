32a1c64df076bffa502d5191ad13b8c7fc46ab6a
Janaeq <janaemassa@gmail.com>
2020-12-13 22:12:11 -0500

    created CRUD routes

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,13 +1,41 @@
 class PatientsController < ApplicationController
 
-    #Index
+    #CREATE 
+    
+    get "/doctors/:id/patients/new" do #IS THIS CONVENTIONNNNNNNN
+        #creates a new patient 
+        #can we specify this route to create a patient for that specific doctor?
+        @doctor_id = params[:id]
+        erb :'patients/new'
+    end
+
+    post '/doctors/:id/patients' do
+        #saves the new patient to the database
+        #redirects to that patient's /patients/:id page
+    end
+    
+    #READ
 
     get '/patients/:id' do
-        #shows patients that belong to a specific doctor
-        #connected by foreign key
-        #limits to one patient :(
+        #shows a specific patient
+        #can lead to link to update patient info
         @patient = Patient.find_by(id: params[:id])
-        # binding.pry
         erb :'patients/show'
+    end
+
+    #UPDATE
+
+    get '/patients/:id/edit' do
+        #renders the form to update a specific doctor's info
+    end
+
+    patch 'patients/:id' do
+        #actually updates the info
+    end
+
+    #DELETE
+
+    delete '/patients/:id' do
+        #deletes this doctor
     end
 end 
